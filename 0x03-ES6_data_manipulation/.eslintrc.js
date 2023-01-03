714604f625efc5a05f1cbe4395b458b5e18306fd
ekan07 <aniekanbon@gmail.com>
2023-01-03 22:19:28 +0100

    Add files: 0-get_list_students.js, 1-get_list_student_ids.js, 2-get_students_by_loc.js, 3-get_ids_sum.js, 4-update_grade_by_city.js, 5-typed_arrays.js

---
+++ 0x03-ES6_data_manipulation/.eslintrc.js
@@ -0,0 +1,37 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+    jest: true,
+  },
+  extends: [
+    'airbnb-base',
+    'plugin:jest/all',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  overrides: [
+    {
+      files: ['*.js'],
+      excludedFiles: 'babel.config.js',
+    }
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['jest'],
+  rules: {
+    'max-classes-per-file': 'off',
+    'no-underscore-dangle': 'off',
+    'no-console': 'off',
+    'no-shadow': 'off',
+    'no-restricted-syntax': [
+      'error',
+      'LabeledStatement',
+      'WithStatement',
+    ],
+  },
+};
