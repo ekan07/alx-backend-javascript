f459a5ea3b351849fd91f13675b2d386092d8a70
ekan07 <aniekanbon@gmail.com>
2023-02-25 01:28:22 +0100

    Add tasks 0 to 7

---
+++ 0x06-unittests_in_js/5-payment.js
@@ -0,0 +1,14 @@
+const Utils = require('./utils');
+
+
+/*
+ * @param {number} totalAmount
+ * @param {number} totalShipping
+ * @return {number} sum
+ */
+function sendPaymentRequestToApi(totalAmount, totalShipping) {
+  const result = Utils.calculateNumber('SUM', totalAmount, totalShipping);
+  console.log(`The total is: ${result}`);
+}
+
+module.exports = sendPaymentRequestToApi;
